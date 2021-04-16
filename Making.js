e9d5d34eceb8a6b8b094bf681bc5fc4013ead31f
won-hyw <80093106+won-hyw@users.noreply.github.com>
2021-04-17 02:45:53 +0900

    Test.html 미디어쿼리 수정, Making.html 파일 수정

--- Making.js
+++ Making.js
@@ -4,35 +4,37 @@ $(document).ready (
     function () {
         $('.add').click (function () {
             if (count < 10){
-                $('.addInput').append (
+                $('.voca_field .addInput').append (
                     '<li>\
-                        00'+(count++)+
-                    ' <input type="text" name="voca" />\
-                    <input type="text" name="mean" />\
-                    </li>'
+                    00'+ count++ + '\
+                    <input type="text" name="voca" /></li>'
                 );
-            }else if (count>100){
-                $('.addInput').append (
+            }else if (count >= 10){
+                $('.voca_field .addInput').append (
                     '<li>\
-                        0'+(count++)+
-                    ' <input type="text" name="voca" />\
-                    <input type="text" name="mean" />\
-                    </li>'
+                    0'+ count++ + '\
+                    <input type="text" name="voca" /></li>'
                 );
             }else{
-                $('.addInput').append (
-                    '<li>'
-                        +(count++)+
-                    ' <input type="text" name="voca" />\
-                    <input type="text" name="mean" />\
-                    </li>'
+                $('.voca_field .addInput').append (
+                    '<li>\
+                    '+ count++ + '\
+                    <input type="text" name="voca" /></li>'
                 );
             } // 추가
+            
+            $('.mean_field .addInput').append (
+                '<li><input type="text" name="mean" /></li>'
+            );
         });
         $('.minus').click (function () {
-            var removeLi = document.querySelectorAll(".voca_making li");
+            var removeLi1 = document.querySelectorAll(".number_field li");
+            var removeLi2 = document.querySelectorAll(".voca_field li");
+            var removeLi3 = document.querySelectorAll(".mean_field li");
             if(count > 1){
-                $(removeLi).last().remove();
+                $(removeLi1).last().remove();
+                $(removeLi2).last().remove();
+                $(removeLi3).last().remove();
                 count--;
             }
         }); // 제거
