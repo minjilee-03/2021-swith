155fdc9ba9f89119e12d17c6caa94e2f1ef4f569
MoonJinjin <s2019s11@e-mirim.hs.kr>
2021-05-01 22:46:04 +0900

    210501

--- MakingVocabulary.js
+++ MakingVocabulary.js
@@ -1,10 +1,24 @@
 $(document).ready(
     function () {
         $('.trash').click(function () {
+            $('.modal').css('display', 'block');
             var remove = $(this).parent().parent();
             var remove_sub = $(this);
-            remove.remove();
-            remove_sub.remove();
+
+            $('.btn_ok').click(function () {
+                remove.remove();
+                remove_sub.remove();
+                $('.modal').css('display', 'none');
+            });
+
+            $('.btn_no').click(function () {
+                $('.modal').css('display', 'none');
+            });
         });
+
+        $('.close').click(function () {
+            $('.modal').css('display', 'none');
+        });
+
     }
 );
