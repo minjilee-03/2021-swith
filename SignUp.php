cdefe16d43bbfcc2b1199d95a58421c6d31d5f75
MoonJinjin <s2019s11@e-mirim.hs.kr>
2021-05-21 15:51:24 +0900

    210521

--- SignUp.php
+++ SignUp.php
@@ -1,32 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-</head>
-<body>
-<?
-$conn = mysqli_connect(주소, 아이디, "비밀번호", DB 스키마 이름, 포트);
-$hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
-echo $hashedPassword;
-$sql = "
-    INSERT INTO user
-    (id, password, name, birth)
-    VALUES('{$_POST['email']}', '{$hashedPassword}', NOW()
-    )";
-echo $sql;
-$result = mysqli_query($conn, $sql);
-
-if ($result === false) {
-    echo "저장에 문제가 생겼습니다. 관리자에게 문의해주세요.";
-    echo mysqli_error($conn);
-} else {
-?>
-    <script>
-        alert("회원가입이 완료되었습니다");
-        location.href = "index.html";
-    </script>
+<meta charset="UTF-8">
 <?php
-}
+    $conn = mysqli_connect("localhost", "mirim", "1234", "swith"); // DB와 연결
+    $hashedPassword = password_hash($_POST['pw'], PASSWORD_DEFAULT); // 암호화
+    $sql = "
+        INSERT INTO user
+        (id, pw, name, year, month, day)
+        VALUES('{$_POST['id']}', '{$hashedPassword}', '{$_POST['name']}', '{$_POST['year']}', '{$_POST['month']}', '{$_POST['day']}'
+        )";
+    $result = mysqli_query($conn, $sql); // Query문 실행
+
+    if ($result === false) {
+        echo "저장에 문제가 생겼습니다. 관리자에게 문의해주세요.";
+        echo mysqli_error($conn);
+    } else {
 ?>
-</body>
-</html>
+
+<script>
+    alert("회원가입이 완료되었습니다");
+    location.href = "index.html";
+</script>
+
+<?php
+    }
+?>
