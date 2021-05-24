c91fc537a39f029ccb062ac59c21e41fe607fd87
won-hyw <80093106+won-hyw@users.noreply.github.com>
2021-05-24 19:00:22 +0900

    버튼 hover 추가 및 + - 버튼 수정

--- Making.js
+++ Making.js
@@ -1,4 +1,4 @@
-var count = 9;
+var count = 9; 
 
 // $(document).on으로 동적요소들이 추가되었을 때 이벤트를 위임한다.
 $(document).on("click", '.add', function () { // .add 버튼을 누르면 요소가 추가된다.
@@ -19,20 +19,24 @@ $(document).on("click", '.add', function () { // .add 버튼을 누르면 요소
 
 $(document).on("click", '.minus', function () { // .minus 버튼을 누르면 요소가 삭제된다.
     if ((count - 1) > 1) { // count가 1보다 작지 않을 때 실행하는 일.
-        if ($(this).parent().parent().is($('li').last())) { // 해당 버튼의 부모의 부묘 요소인 li 태그가 마지막 li태그의 요소인지 확인한다.
+        if($(this).parent().parent().is($('li').last())){ // 해당 버튼의 부모의 부묘 요소인 li 태그가 마지막 li태그의 요소인지 확인한다.
             $(this).parent().parent().prev().find('input[name="voca"]').focus(); // 삭제하기 전에 전 li태그의 input을 포커스한다.
-        } else {
+        }else{
             $(this).parent().parent().next().find('input[name="voca"]').focus(); // 삭제하기 전에 다음 li태그의 input을 포커스한다.
-            $(this).parent().parent().next().find('.addMinus').css('top', ($(this).parent().parent().find('input').offset().top) + 20); // 버튼 위치를 조정한다.
-        }
+            $(this).parent().parent().next().find('.addMinus').css('top', ($(this).parent().parent().find('input').offset().top)+20); // 버튼 위치를 조정한다.
+        }  
         $(this).parent().parent().remove(); // 해당 버튼의 부모의 부모 요소인 li 태그가 삭제된다.
         count--; // 숫자 감소
     }
-    $('.num').each(function (idx) {
-        $(this).text(('00' + (idx + 1)).slice(-3));
+    $('.num').each(function(idx) {
+        $(this).text(('00' + (idx+1)).slice(-3));
     });
 }); // 제거
 
-$(document).on("focus", 'input:not([class="hidden_text"])', function () {
-    $('.addMinus').css('top', ($(this).offset().top) + 20);
+$(document).on("focus", 'input:not([class="hidden_text"])', function (){
+    $('.addMinus').css('top', ($(this).offset().top)+20);
 }); // input 태그의 y좌표값 + 20을 .addMinus 태그의 css에 적용한다. (단, hidden_text를 클래스로 가진 input은 제외한다)
+
+$(document).on("focus", '#title', function (){
+    $('.addMinus').css('top', ($('.basic_input').offset().top)+20);
+});
